0432738ef51850524b25edfd1ac1f2d7467f7391
Jayhua <jordanhua97@gmail.com>
2018-10-01 19:25:20 -0700

    Updates

---
+++ precache-manifest.6ca1255337ca78361f1d4702239d0392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky_game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd845ee937b9e5f2a5a2",
+    "url": "/clicky_game/static/js/runtime~main.bd845ee9.js"
+  },
+  {
+    "revision": "e25d41f5512d80a65843",
+    "url": "/clicky_game/static/js/main.e25d41f5.chunk.js"
+  },
+  {
+    "revision": "343dc2be3e4dc0312709",
+    "url": "/clicky_game/static/js/1.343dc2be.chunk.js"
+  },
+  {
+    "revision": "e25d41f5512d80a65843",
+    "url": "/clicky_game/static/css/main.886cbe3a.chunk.css"
+  },
+  {
+    "revision": "7b68f13cfd91927c1ebe56510569f803",
+    "url": "/clicky_game/index.html"
+  }
+];
